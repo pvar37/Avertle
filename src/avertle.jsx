4cc6be2ec10adafa90cf797db6b103f8dd3a30e1
Pranay Varada <52256079+pvar37@users.noreply.github.com>
2025-07-28 08:01:05 -0400

    Updating footer

--- src/avertle.jsx
+++ src/avertle.jsx
@@ -749,7 +749,7 @@ export default function AvertleGame() {
           </div>
         )}
         <footer style={styles.footer}>
-          &copy; 2025 <a href="https://www.pranayvarada.com" style={{ color: 'white', textDecoration: 'underline' }}>Pranay Varada</a>. All rights reserved.
+          A game by <a href="https://www.pranayvarada.com" style={{ color: 'white', textDecoration: 'underline' }}>Pranay Varada</a>
         </footer>
 
       </div>
@@ -906,7 +906,7 @@ export default function AvertleGame() {
           />
         )}
         <footer style={styles.footer}>
-          &copy; 2025 <a href="https://www.pranayvarada.com" style={{ color: 'white', textDecoration: 'underline' }}>Pranay Varada</a>. All rights reserved.
+          A game by <a href="https://www.pranayvarada.com" style={{ color: 'white', textDecoration: 'underline' }}>Pranay Varada</a>
         </footer>
       </div>
       </>
